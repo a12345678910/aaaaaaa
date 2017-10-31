ed3ac34b732f88ffb1a458ad87c305ccd6a7e5e1
gq <1057386657@qq.com>
2017-10-31 13:17:37 +0800

    这是修改

--- app/src/main/java/com/example/gq/aaaaaaa/MainActivity.java
+++ app/src/main/java/com/example/gq/aaaaaaa/MainActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //isdhashdasdasdasda穷三代
+
     }
 }
